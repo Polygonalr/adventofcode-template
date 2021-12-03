886fc7730d4917fad4155e4f58290def12161484
Zhi Heng <yapzhhg@gmail.com>
2021-12-03 13:36:14 +0800

    Minor fixes

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn p2(line_vec: Vec<String>) -> i32 {
 fn main() {
     let filepath = "./input.txt";
     // let mut str_buf = "".to_owned();
-    let line_vec: Vec<&str> = Vec::new();
+    let mut line_vec: Vec<String> = Vec::new();
     if let Ok(lines) = read_lines(filepath) {
         for line in lines {
             if let Ok(s) = line {
